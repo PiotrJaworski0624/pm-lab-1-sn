51a290ae644683d9b4100e2e2b0b59efc26668e7
Piotr Jaworski <piotr.jaworski0624@gmail.com>
2021-01-16 13:36:12 +0100

    Wykonano zadanie 2.2.1a

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
 }
